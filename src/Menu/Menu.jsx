502db1afab3d386f778f0efeb236d2a8358f2b3f
gitHindu <hindureddy2002@gmail.com>
2025-02-05 17:54:58 +0530

    Update Menu.jsx
    css change

--- src/Menu/Menu.jsx
+++ src/Menu/Menu.jsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Link } from "react-router-dom";
-import "./menu.css";
+import "./Menu.css";
 
 const Menu = () => {
   return (
